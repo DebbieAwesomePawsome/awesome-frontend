c88c2426e983e94afb8282d86d2015b7c446ae1b
LaffCollie <laffcollie@gmail.com>
2025-05-25 17:51:02 +1000

    Clean up frontend: branded homepage, updated title, proper .gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
 
-
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [ // <-- ADD THIS SECTION
+    'm-12',
+    'p-12',
+    'font-extrabold',
+    'bg-blue-700',
+    'text-yellow-300',
+    'text-5xl',
+    // Let's also add one of your original test colors
+    'text-pink-500',
+    'underline'
+  ],
   theme: {
-    extend: {
-      colors: {
-        ...defaultTheme.colors
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
